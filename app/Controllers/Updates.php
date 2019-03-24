2b7822ec15c17602291e829ef6642fabac862af9
nguyen_dttn <contact.wilcity@gmail.com>
2019-03-24 22:45:25 +0700

    Changed Prevview To ChangeLog button

--- app/Controllers/Updates.php
+++ app/Controllers/Updates.php
@@ -424,7 +424,7 @@ class Updates {
 			    <?php elseif (General::isNewVersion($this->aTheme['version'], $this->oCurrentThemeVersion->get('Version'))): ?>
                     <div class="ui basic green button"><a class="wil-update-theme">Update</a></div>
 			    <?php endif; ?>
-                <div class="ui basic red button"><a target="_blank" href="<?php echo esc_url($this->aTheme['url']); ?>">Preview</a></div>
+                <div class="ui basic red button"><a target="_blank" href="<?php echo esc_url($this->aTheme['url']); ?>">Changelog</a></div>
             </div>
         </div>
 	    <?php
@@ -439,7 +439,7 @@ class Updates {
                 <?php elseif (General::isNewVersion($aNewPlugin['version'], $aCurrentPluginInfo['Version'])): ?>
                     <div class="ui basic green button"><a class="wil-update-plugin" href="<?php echo esc_url($this->updatechangeLogURL($aNewPlugin['slug'])); ?>">Update</a></div>
                 <?php endif; ?>
-                <div class="ui basic red button"><a target="_blank" href="<?php echo esc_url($this->getPreviewURL($aNewPlugin)); ?>">Preview</a></div>
+                <div class="ui basic red button"><a target="_blank" href="<?php echo esc_url($this->getPreviewURL($aNewPlugin)); ?>">Changelog</a></div>
             </div>
         </div>
         <?php
